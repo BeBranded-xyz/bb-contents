53ab1a0ee50c98767c188411ed2f55087cbfcd72
Roland <roland@MacBook-Pro-de-Roland.local>
2025-12-18 12:18:04 +0100

    Bump version to 1.0.101

--- bb-contents.js
+++ bb-contents.js
@@ -1,7 +1,7 @@
 /**
  * BeBranded Contents
  * Contenus additionnels français pour Webflow
- * @version 1.0.100
+ * @version 1.0.101
  * @author BeBranded
  * @license MIT
  * @website https://www.bebranded.xyz
@@ -32,11 +32,11 @@
     window._bbContentsInitialized = true;
 
     // Log de démarrage simple (une seule fois)
-    console.log('bb-contents | v1.0.100');
+    console.log('bb-contents | v1.0.101');
 
     // Configuration
     const config = {
-        version: '1.0.100',
+        version: '1.0.101',
         debug: false, // Debug désactivé pour rendu propre
         prefix: 'bb-', // utilisé pour générer les sélecteurs (data-bb-*)
         youtubeEndpoint: null, // URL du worker YouTube (à définir par l'utilisateur)
