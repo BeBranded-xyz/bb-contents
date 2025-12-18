e74c99a53288404bf847dc7acb959cd5085e896c
Roland <roland@MacBook-Pro-de-Roland.local>
2025-12-18 14:23:34 +0100

    Fix Safari: supprimer appel waitForImages() inexistant

--- bb-contents.js
+++ bb-contents.js
@@ -1,7 +1,7 @@
 /**
  * BeBranded Contents
  * Contenus additionnels français pour Webflow
- * @version 1.0.101
+ * @version 1.0.102
  * @author BeBranded
  * @license MIT
  * @website https://www.bebranded.xyz
@@ -32,11 +32,11 @@
     window._bbContentsInitialized = true;
 
     // Log de démarrage simple (une seule fois)
-    console.log('bb-contents | v1.0.101');
+    console.log('bb-contents | v1.0.102');
 
     // Configuration
     const config = {
-        version: '1.0.101',
+        version: '1.0.102',
         debug: false, // Debug désactivé pour rendu propre
         prefix: 'bb-', // utilisé pour générer les sélecteurs (data-bb-*)
         youtubeEndpoint: null, // URL du worker YouTube (à définir par l'utilisateur)
@@ -587,9 +587,6 @@
                         element.addEventListener('mouseleave', () => isPaused = false);
                     }
                 };
-                
-                // Démarrer le processus de chargement des images
-                waitForImages();
             },
 
             initStandardAnimation: function(element, scrollContainer, mainBlock, options) {
